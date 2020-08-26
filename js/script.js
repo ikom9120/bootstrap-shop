bcbb8ba708d6eb3cef4e3969bdf29f26c9c664a6
ikom9120 <ikom9120@gmail.com>
2020-08-26 17:21:27 +0300

    BS-9 добавлена динамика на кнопку Купить при просмотре товара

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#buy-button').on('click', function (e) {
+
+
+        $('#buy-button').hide();
+        $('#buy-div').html('<span class="buy-text">Товар уже в корзине</div>');
+    });
+
+});
+
